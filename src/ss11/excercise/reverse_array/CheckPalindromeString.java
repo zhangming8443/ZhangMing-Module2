c60603cebdb084b13dd35979fe01ee4a8a52d1fd
Truong Van Hue Minh <tvhueminh2kdn@gmail.com>
2023-06-17 12:01:09 +0700

    update

--- src/ss11/excercise/reverse_array/CheckPalindromeString.java
+++ src/ss11/excercise/reverse_array/CheckPalindromeString.java
@@ -1,7 +1,32 @@
 package ss11.excercise.reverse_array;
 
-import java.util.Queue;
+import java.util.*;
 
-//public class CheckPalindromeString {
-//    Queue
-//}
+public class CheckPalindromeString {
+    public static void main(String[] args) {
+        String arr = "Able was I ere I saw Elba";
+        String str[] = arr.split("");
+        Stack<String> stack = new Stack<>();
+        Queue<String> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < str.length; i++) {
+            stack.push(str[i]);
+            queue.add(str[i]);
+        }
+        System.out.println(Arrays.toString(str));
+        boolean flag = true;
+        while (!stack.isEmpty()) {
+            if (Objects.equals(stack.pop(), queue.remove())) {
+                flag = true;
+            } else {
+                flag = false;
+                System.out.println(false);
+                break;
+            }
+        }
+        if (flag) {
+            System.out.println(true);
+        }
+    }
+
+}
